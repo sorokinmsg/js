21e2406c5e9e367365b55e2e2606c0206559bfe4
Weiss <sorokinmsg@gmail.com>
2019-09-26 17:27:10 +0300

    k

---
+++ 1.go
@@ -0,0 +1,37 @@
+package main
+import "fmt"
+
+func main()  {
+
+var age = 12
+var num = 2.123
+var str = "Hello World"
+
+fmt.Println(age, num, str)
+
+var a = 10
+
+var b = 5
+var res int
+res = a + b
+fmt.Println("Result is ", res)
+
+
+//const pi = 3.14
+
+var web string = "itProger"
+fmt.Println(web + " is cool web \nsite")
+
+
+
+var nums float64 = 4.1231253
+fmt.Printf("%.2f  \n", nums)
+
+var isDone bool = true
+fmt.Printf("%T \n", isDone)
+
+var hal = "Second one"
+fmt.Printf("%X \n", hal)
+
+
+}
